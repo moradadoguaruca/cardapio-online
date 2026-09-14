0b559f8348c1aa17f033f22c42f1134485876d6f
Diego - Bar Guaruçá <diego.barguaruca@gmail.com>
2026-09-14 10:53:45 -0300

    Update i18n.js
    actualización de tags

--- js/i18n.js
+++ js/i18n.js
@@ -29,7 +29,7 @@ const UI = {
     },
     footer: {
       instagram: "Instagram",
-      website: "Links da Morada"
+      website: "Conheça a Morada"
     },
     langSwitch: {
       label: "Idioma"
@@ -41,7 +41,7 @@ const UI = {
       label: "Volver al inicio"
     },
     footerNote: {
-      tax: "Se adicionará un 10% de cargo por servicio a la cuenta."
+      tax: "Un recargo del 10% será adicionado a la cuenta."
     },
     
     roomService: {
@@ -54,7 +54,7 @@ const UI = {
     },
     footer: {
       instagram: "Instagram",
-      website: "Links de Morada"
+      website: "Explorá la Morada"
     },
     langSwitch: {
       label: "Idioma"
@@ -81,7 +81,7 @@ const UI = {
     },
     footer: {
       instagram: "Instagram",
-      website: "Morada's links"
+      website: "Experience Morada"
     },
     langSwitch: {
       label: "Language"
